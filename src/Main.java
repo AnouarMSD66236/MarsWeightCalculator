ca08e2c84696f1bceabf461340f1e6f3459dd7de
AnouarMSD66236 <anouarmoussaid21@gmail.com>
2024-03-02 13:02:09 +0100

    java project . third

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Date;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Date time = new Date();
+        System.out.println(time);
+        System.out.println("Hi how are you ");
+        
+
+
+    }
+}
